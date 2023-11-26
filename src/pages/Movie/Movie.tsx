70a2ce29160816803237c89735679c54d0139201
vitor kravszenko <vitorkravs3@gmail.com>
2023-11-26 18:37:49 -0300

    feat: componente movie e movie css

--- src/pages/Movie/Movie.tsx
+++ src/pages/Movie/Movie.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
 
-const Movie = () => {
-  return <div>Movie</div>;
+import MovieCard from "../../components/MovieCard/MovieCard";
+
+import "./Movie.css";
+
+interface Movie {
+  id: number;
+  title: string;
+  vote_average: number;
+  poster_path: string;
+  tagline: string;
+  budget: number;
+  revenue: number;
+  runtime: number;
+  overview: string;
+  // Adicione outros campos conforme necessário
+}
+
+const moviesURL: string = import.meta.env.VITE_API;
+const apiKey: string = import.meta.env.VITE_API_KEY;
+
+const Movie: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  const getMovie = async (url: string): Promise<void> => {
+    try {
+      const res = await fetch(url);
+      const data: Movie = await res.json();
+      console.log(data);
+      setMovie(data);
+    } catch (error) {
+      console.error("Error fetching movie:", error);
+    }
+  };
+
+  const formatCurrency = (number: number): string => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    getMovie(movieUrl);
+  }, [id]);
+
+  return (
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 /> Orçamento:
+            </h3>
+            <p>{formatCurrency(movie.budget)}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>{formatCurrency(movie.revenue)}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+          <div className="info description">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Movie;
