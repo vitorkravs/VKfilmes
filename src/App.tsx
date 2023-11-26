55c191577f67a98964805dbc8b52a8603daecaf6
vitor kravszenko <vitorkravs3@gmail.com>
2023-11-26 12:29:51 -0300

    feat: componente Navbar e seus estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Outlet } from "react-router-dom";
+
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import React, { useState, FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./Navbar.css";
+
+const Navbar: React.FC = () => {
+  const [search, setSearch] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> VKfilmes
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
